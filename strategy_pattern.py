6cc8c569cd7d2edf095ee6b9c44c808645a0e205
voidnologo <(none)>
2020-02-24 20:46:30 -0500

    strategy pattern: more generic

--- strategy_pattern.py
+++ strategy_pattern.py
@@ -4,51 +4,51 @@ import abc
 # -----------------------------------------------
 class IQuackBehavior(abc.ABC):
     @abc.abstractmethod
-    def quack():
+    def run():
         pass
 
 
 class LoudQuacking(IQuackBehavior):
-    def quack(self):
+    def run(self):
         print(f'QUACK: {self.__class__.__name__}')
 
 
 class NoQuacking(IQuackBehavior):
-    def quack(self):
+    def run(self):
         print(f'QUACK: {self.__class__.__name__}')
 
 
 # -----------------------------------------------
 class IFlyBehavior(abc.ABC):
     @abc.abstractmethod
-    def fly():
+    def run():
         pass
 
 
 class SimpleFlying(IFlyBehavior):
-    def fly(self):
+    def run(self):
         print(f'FLY: {self.__class__.__name__}')
 
 
 class JetFlying(IFlyBehavior):
-    def fly(self):
+    def run(self):
         print(f'FLY: {self.__class__.__name__}')
 
 
 # -----------------------------------------------
 class IDisplayBehavior(abc.ABC):
     @abc.abstractmethod
-    def display():
+    def run():
         pass
 
 
 class PrintDisplaying(IDisplayBehavior):
-    def display(self):
+    def run(self):
         print(f'DISPLAY: {self.__class__.__name__}')
 
 
 class ScreenDisplaying(IDisplayBehavior):
-    def display(self):
+    def run(self):
         print(f'DISPLAY: {self.__class__.__name__}')
 
 
@@ -60,22 +60,14 @@ class Duck:
         'display': IDisplayBehavior,
     }
 
-    def __init__(self, quack, fly, display):
-        assert issubclass(quack, Duck.behaviors['quack'])
-        assert issubclass(fly, Duck.behaviors['fly'])
-        assert issubclass(display, Duck.behaviors['display'])
-        self._quack = quack
-        self._fly = fly
-        self._display = display
+    def __new__(cls, *args, **kwargs):
+        assert issubclass(cls.behaviors['quack'], Duck.behaviors['quack'])
+        assert issubclass(cls.behaviors['fly'], Duck.behaviors['fly'])
+        assert issubclass(cls.behaviors['display'], Duck.behaviors['display'])
+        return super().__new__(cls, *args, **kwargs)
 
-    def quack(self):
-        self._quack().quack()
-
-    def fly(self):
-        self._fly().fly()
-
-    def display(self):
-        self._display().display()
+    def __getattr__(self, attr):
+        return self.behaviors.get(attr)().run
 
 
 class MountainDuck(Duck):
@@ -85,9 +77,6 @@ class MountainDuck(Duck):
         'display': PrintDisplaying,
     }
 
-    def __init__(self):
-        super().__init__(**self.behaviors)
-
 
 class CloudDuck(Duck):
     behaviors = {
@@ -95,9 +84,6 @@ class CloudDuck(Duck):
         'fly': JetFlying,
         'display': PrintDisplaying,
     }
-
-    def __init__(self):
-        super().__init__(**self.behaviors)
 
 
 class RubberDuck(Duck):
@@ -107,27 +93,11 @@ class RubberDuck(Duck):
         'display': ScreenDisplaying,
     }
 
-    def __init__(self):
-        super().__init__(**self.behaviors)
-
 
 # -----------------------------------------------
-print('\n\n <<< rubber duck >>>')
-rubber_duck = RubberDuck()
-rubber_duck.quack()
-rubber_duck.fly()
-rubber_duck.display()
-
-
-print('\n\n <<< mountain duck >>>')
-mountain_duck = MountainDuck()
-mountain_duck.quack()
-mountain_duck.fly()
-mountain_duck.display()
-
-
-print('\n\n <<< cloud duck >>>')
-cloud_duck = CloudDuck()
-cloud_duck.quack()
-cloud_duck.fly()
-cloud_duck.display()
+flock = (RubberDuck(), MountainDuck(), CloudDuck())
+for duck in flock:
+    print(f'\n\n  <<< {duck.__class__.__name__} >>>')
+    duck.quack()
+    duck.fly()
+    duck.display()
